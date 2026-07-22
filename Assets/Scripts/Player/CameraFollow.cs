e9fe8754ab312ba015594df7dc6314aef64d9f0e
Plaonder <39774970+Plaonder@users.noreply.github.com>
2026-07-22 13:19:29 -0700

    Like actual player movement and stuff

---
+++ Assets/Scripts/Player/CameraFollow.cs
@@ -0,0 +1,50 @@
+using NUnit.Framework.Interfaces;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private GameObject target;
+    [SerializeField] private float baseFollowSpeed = 10.0f;
+    [SerializeField] private float returnToBaseFollowSpeed = 2.0f;
+    [SerializeField] private float defaultCameraShakeSpeed = 0.02f;
+    
+    private float _followSpeed;
+    private float _shakeAmount;
+    private float _shakeResetTime;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (target == null)
+        {
+            target = GameObject.FindGameObjectWithTag("Player");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Return follow speed to default
+        _followSpeed = Mathf.Lerp(_followSpeed, baseFollowSpeed, Time.deltaTime * returnToBaseFollowSpeed);
+        _shakeAmount = Mathf.Lerp(_shakeAmount, 0, Time.deltaTime * _shakeResetTime);
+        
+        // Move to target
+        Vector3 targetPosition = Vector3.Lerp(transform.position, target.transform.position, _followSpeed * Time.deltaTime);
+        
+        targetPosition += Random.insideUnitSphere * _shakeAmount;
+        // Set the position without changing the Z
+        transform.position = new Vector3(targetPosition.x, targetPosition.y, transform.position.z);
+    }
+
+    public void FreezeCameraTemporarily()
+    {
+        // I love me some hard coded numbers
+        _followSpeed = 0.0f;
+    }
+
+    public void StartCameraShake(float shakeAmount = 0.02f, float shakeResetTime = 0.5f)
+    {
+        _shakeAmount = shakeAmount;
+        _shakeResetTime = shakeResetTime;
+    }
+}
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -3,26 +3,49 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Objects")]
+    [SerializeField] private GameObject cameraPosition;
+    
+    [Header("Movement")]
     [SerializeField] private float baseMoveSpeed = 250.0f;
+    
+    [Header("Dashing")]
     [SerializeField] private float dashVelocity = 20.0f;
+    [SerializeField] private float dashCooldown = 0.5f;
     [SerializeField] private float minimumVelocityToDash = 0.05f;
+    [SerializeField] private float dashCameraShakeAmount = 0.1f;
+    [SerializeField] private float dashCameraShakeResetSpeed = 5.0f;
+    
+    [Header("Sound Effects")]
+    [SerializeField] private AudioSource dashSound;
     
     private Vector2 _input;
     private Rigidbody2D _rb;
+    private CameraFollow _cameraFollowScript;
+    private float _dashCooldownTime = 0.0f;
+    
     
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+
+        if (cameraPosition == null)
+        {
+            cameraPosition = GameObject.FindGameObjectWithTag("MainCamera");
+        }
+        
+        _cameraFollowScript = cameraPosition.GetComponent<CameraFollow>();
     }
 
     void Update()
     {
-
+        _dashCooldownTime -= Time.deltaTime;
+        _dashCooldownTime = Mathf.Clamp(_dashCooldownTime, 0, dashCooldown);
     }
 
     void FixedUpdate()
     {
-        _rb.AddForce(_input * baseMoveSpeed, ForceMode2D.Force);
+        _rb.AddRelativeForce(_input * baseMoveSpeed, ForceMode2D.Force);
     }
 
     public void OnMove(InputAction.CallbackContext context)
@@ -30,13 +53,31 @@ public class PlayerMovement : MonoBehaviour
         _input = context.ReadValue<Vector2>();
     }
 
-    public void OnDash()
+    public void OnDash(InputAction.CallbackContext context)
     {
-        if (_rb.linearVelocity.magnitude < minimumVelocityToDash)
+        // Don't do it again when we let go!
+        if (context.canceled)
         {
             return;
         }
         
-        _rb.AddForce(_input * dashVelocity, ForceMode2D.Impulse);
+        // Don't do it if we're moving too slow, or the cooldown hasn't finished.
+        if (_rb.linearVelocity.magnitude < minimumVelocityToDash || _dashCooldownTime > 0.0f)
+        {
+            return;
+        }
+        
+        // Freeze Camera for a sec and shake
+        _cameraFollowScript.FreezeCameraTemporarily();
+        _cameraFollowScript.StartCameraShake(dashCameraShakeAmount, dashCameraShakeResetSpeed);
+        
+        // Move!
+        _rb.AddRelativeForce(_input * dashVelocity, ForceMode2D.Impulse);
+        
+        // Set the cooldown
+        _dashCooldownTime = dashCooldown;
+        
+        // Sound effect!
+        dashSound.Play();
     }
 }
