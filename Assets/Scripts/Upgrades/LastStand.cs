9e0ba6541af1e56471c2a9bcf30c371014ed6c9a
RogueFirework <70032201+roguefirework@users.noreply.github.com>
2026-07-23 13:08:00 -0700

    Last stand

---
+++ Assets/Scripts/Upgrades/LastStand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Player;
+using UnityEngine;
+
+namespace Upgrades
+{
+    public class LastStand : MonoBehaviour
+    {
+        private Modifier activeModifier;
+        [SerializeField] private float start;
+        private bool active = false;
+        public void Update()
+        {
+            if (Player.Player.Instance.TimeEntity.GetTime() < start && !active)
+            {
+                active = true;
+                activeModifier =
+                    Player.Player.Instance.PlayerModifier.AddModifier(PlayerStat.Damage, ModifierType.Multiplicative,
+                        3.0f, this);
+            }
+            else if (active && Player.Player.Instance.TimeEntity.GetTime() > start)
+            {
+                active = false;
+                Player.Player.Instance.PlayerModifier.RemoveModifiersFromSource(this);
+            }
+        }
+    }
+}
