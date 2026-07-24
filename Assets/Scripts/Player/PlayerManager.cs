35e2eb868e13d9f05d4af30a69c410c74693df6c
RogueFirework <70032201+roguefirework@users.noreply.github.com>
2026-07-24 14:52:14 -0700

    Add player death

--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Player
 {
@@ -6,10 +8,22 @@ namespace Player
     {
         public static PlayerManager Instance;
         [SerializeField] public Camera playerCamera;
+        [SerializeField] public SpriteRenderer bigBlackBox;
 
         public void Awake()
         {
             Instance = this;
+        }
+
+        public void LoadSceneAfterDelay(string scene, float delay)
+        {
+            StartCoroutine(LoadScene(scene,delay));
+        }
+
+        private IEnumerator LoadScene(string scene, float delay)
+        {
+            yield return new WaitForSeconds(delay);
+            SceneManager.LoadScene(scene);
         }
     }
 }
