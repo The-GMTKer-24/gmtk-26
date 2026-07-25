55971f0288dc1864041a73a89b6d9d65bda5868a
MaximumMaxx <maxwellwindland@gmail.com>
2026-07-25 09:58:55 -0700

    Fred exists

--- Assets/Scripts/Dungeon/TutorialFred.cs
+++ Assets/Scripts/Dungeon/TutorialFred.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -9,29 +10,71 @@ public class TutorialFred : MonoBehaviour
 {
     [SerializeField] private TMP_Text textField;
     [SerializeField] private GameObject interactPrompt;
+    [SerializeField] private GameObject textBox;
     
     [SerializeField] private InputAction interactAction;
+    
+    [SerializeField] private List<string> privateDialoguesList;
 
+    [SerializeField] private string exhaustedDialogueText;
+
+    private int lineCount;
+    private int currentLine;
     private bool isTalking;
+    private bool held;
+
+    private void Start()
+    {
+        lineCount = privateDialoguesList.Count;
+        textField.text = privateDialoguesList[0];
+    }
 
     private void OnEnable() => interactAction.Enable();
     private void OnDisable() => interactAction.Disable();
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (!isTalking)
-        {
-            interactPrompt.SetActive(true);
-        }
+        interactPrompt.SetActive(true);
     }
 
     private void OnTriggerStay2D(Collider2D other)
     {
         if (interactAction.IsPressed())
         {
-            interactPrompt.SetActive(false);
-            textField.gameObject.SetActive(true);
-            
-            // Starting point for the rest of the stuff where you show the text and iterate through it
+            if (isTalking && !held)
+            {
+                held = true;
+                if (currentLine == lineCount)
+                {
+                    textField.text = exhaustedDialogueText;
+                }
+                else
+                {
+                    textField.text = privateDialoguesList[currentLine];
+                    currentLine++;
+                }
+            }
+            else
+            {
+                interactPrompt.SetActive(false);
+                textBox.SetActive(true);
+                isTalking = true;
+            }
         }
+        else
+        {
+            held = false;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        HideAllUI();
+        isTalking = false;
+    }
+
+    void HideAllUI()
+    {
+        interactPrompt.SetActive(false);
+        textBox.SetActive(false);
     }
 }
