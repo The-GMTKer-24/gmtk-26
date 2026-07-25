353f3b06dad8088acd4d827533592bc51db2558f
RogueFirework <70032201+roguefirework@users.noreply.github.com>
2026-07-25 00:26:03 -0700

    Boss

--- Assets/Scripts/Boss/Boss.cs
+++ Assets/Scripts/Boss/Boss.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using Entity;
 using UnityEngine;
@@ -12,15 +13,19 @@ namespace Boss
         [Header("Stats")]
         [SerializeField] private List<AttackInfo> attackStats;
         [SerializeField] private float attackCooldown;
+        [Header("Bullet Wave")]
+        [SerializeField] private float bulletWaveOffsetTime;
+        [SerializeField] private float bulletWaveVelocity;
+        [SerializeField] private int bulletWaveBullets;
         [Header("Spawn Locations")]
         [SerializeField] private List<Transform> clockBoulderPositions;
         [SerializeField] private Transform bulletWavePosition;
         [SerializeField] private List<Transform> bulletShipPositions;
 
         [Header("Attack Prefabs")] 
-        //[SerializeField] private ClockBoulder clockBoulder;
+        [SerializeField] private ClockBoulder clockBoulder;
         [SerializeField] private EnemyBullet bossBullet;
-        //[SerializeField] private BulletShips bulletShip;
+        [SerializeField] private BulletShip bulletShip;
         [SerializeField] private Bomb bomb;
         
         private AttackInfo currentAttack;
@@ -49,6 +54,7 @@ namespace Boss
                 }
                 else
                 {
+                    cooldownTimer = attackCooldown;
                     attacking = false;
                 }
             }
@@ -57,18 +63,72 @@ namespace Boss
         private void StartAttack()
         {
             attacking = true;
+            
             currentAttack = attackStats[Random.Range(0, attackStats.Count)];
+            attackTimer = currentAttack.duration;
             TriggerAttack();
         }
 
         private void TriggerAttack()
+        {
+            switch (currentAttack.attack)
+            {
+                case Attack.ClockBoulder:
+                    ClockBoulder(currentAttack);
+                    break;
+                case Attack.BulletWave:
+                    BulletWave(currentAttack);
+                    break;
+                case Attack.BulletShips:
+                    BulletShips(currentAttack);
+                    break;
+                case Attack.Bombs:
+                    Bombs(currentAttack);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        private void Bombs(AttackInfo info)
+        {
+            
+        }
+
+        private void BulletShips(AttackInfo info)
+        {
+            
+        }
+
+        private void BulletWave(AttackInfo info)
+        {
+            IEnumerator Wave(AttackInfo info)
+            {
+                for (int i = 0; i < bulletWaveBullets; i++)
+                {
+                    EnemyBullet bullet = Instantiate(bossBullet, bulletWavePosition.position, Quaternion.identity, transform);
+                    bullet.damage = info.damage;
+                    Vector2 target = Player.Player.Instance.RigidBody.position;
+                    bullet.velocity = bulletWaveVelocity *
+                                      (target - (Vector2)bulletWavePosition.position).normalized;
+                    bullet.remainingTime = 10000;
+                    bullet.GetComponent<Rigidbody2D>().rotation =
+                        -Vector2.SignedAngle(target - (Vector2)transform.position, Vector2.up);
+                    
+                    yield return new WaitForSeconds(bulletWaveOffsetTime);
+                }
+            }
+            StartCoroutine(Wave(info));
+        }
+
+        private void ClockBoulder(AttackInfo info)
         {
             
         }
     }
     
     
-    [System.Serializable]
+    [Serializable]
     struct AttackInfo
     {
         public Attack attack;
---
+++ Assets/Scripts/Boss/Rotator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+namespace Boss
+{
+    public class Rotator : MonoBehaviour
+    {
+        [SerializeField]
+        private float amount;
+        [SerializeField]
+        private float speed;
+
+        private float direction = 1;
+
+        public void Update()
+        {
+            float angle = transform.localRotation.eulerAngles.z + speed * direction * Time.deltaTime;
+            if (Mathf.Abs(Fix(angle)) > amount)
+            {
+                direction *= -1;
+            }
+            transform.localRotation = Quaternion.Euler(0,0,angle);
+        }
+
+        private float Fix(float angle)
+        {
+            return angle < 180 ? angle : angle - 360;
+        }
+    }
+}
