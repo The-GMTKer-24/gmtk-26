591beaae3f582ba3ae9f5a89894ee17db3a06bbb
Plaonder <39774970+Plaonder@users.noreply.github.com>
2026-07-26 01:33:20 -0700

    Intro Sequence

---
+++ Assets/Scripts/UI/Cutscene/CutsceneFrame.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.Cutscene
+{
+    [RequireComponent(typeof(Image))]
+    public class CutsceneFrame : MonoBehaviour
+    {
+        [NonSerialized] public Image cutsceneImage;
+        public float fadeDelay = 5;
+
+        public void Awake()
+        {
+            cutsceneImage = GetComponent<Image>();
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Cutscene/CutsceneManager.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UI.Cutscene
+{
+    public class CutsceneManager : MonoBehaviour
+    {
+        [SerializeField] private CutsceneFrame[] cutsceneFrames;
+        [SerializeField] private float fadeDuration;
+        [SerializeField] private string nextSceneToLoad = "Main";
+    
+        private int currentCutsceneIndex = -1;
+        private float fadeTimer;
+
+        private void Start()
+        {
+            for (int i = 0; i < cutsceneFrames.Length; i++)
+            {
+                cutsceneFrames[i].cutsceneImage.CrossFadeAlpha(0f, 0f, true);
+                cutsceneFrames[i].cutsceneImage.gameObject.SetActive(false);
+            }
+            fadeTimer = 0.1f;
+
+            if (MusicManager.Instance != null)
+            {
+                MusicManager.Instance.SetVolume(0);
+            }
+        }
+    
+        // Update is called once per frame
+        void Update()
+        {
+            fadeTimer -= Time.deltaTime;
+            if (fadeTimer >= 0)
+            {
+                return;
+            }
+
+            currentCutsceneIndex++;
+        
+            if (currentCutsceneIndex >= cutsceneFrames.Length)
+            {
+                if (MusicManager.Instance != null)
+                {
+                    MusicManager.Instance.SetVolume(1);
+                }
+                SceneManager.LoadScene(nextSceneToLoad);
+                return;
+            }
+
+            // Crossfade between the two images
+            if (currentCutsceneIndex > 0)
+            {
+                cutsceneFrames[currentCutsceneIndex - 1].cutsceneImage.CrossFadeAlpha(0f, fadeDuration, false);                
+            }
+            cutsceneFrames[currentCutsceneIndex].cutsceneImage.gameObject.SetActive(true);
+            cutsceneFrames[currentCutsceneIndex].cutsceneImage.CrossFadeAlpha(1f, fadeDuration, false);
+        
+            fadeTimer = cutsceneFrames[currentCutsceneIndex].fadeDelay;
+        }
+
+        public void SkipCutscene()
+        {
+            SceneManager.LoadScene(nextSceneToLoad);
+        }
+    }
+}
